6229d914aad648d0a01a4d7fd9bbf1e07f5088d2
jayantwaldia <imjayantwaldia@gmail.com>
2022-09-15 14:07:46 +0530

    project setup w/ firebase

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Explore />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/profile" element={<SignIn />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<Explore />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqxPbwcljbYyznHNPpPuT2TioGIiiURdE",
+  authDomain: "rent-my-place.firebaseapp.com",
+  projectId: "rent-my-place",
+  storageBucket: "rent-my-place.appspot.com",
+  messagingSenderId: "323789075882",
+  appId: "1:323789075882:web:6ca61bec5d06a22eb9e7d3",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFireStore();
